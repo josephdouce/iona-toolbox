ca34a6ebe44f319949f761eac44c39ac265d8186
joseph douce <josephdouce@gmail.com>
2020-10-21 12:29:00 +0200

    added data

--- js/main.js
+++ js/main.js
@@ -8,45 +8,53 @@ let deferredPrompt = null;
 
 var menuStructure = {
   label: "Home",
-  sub: [{
+  sub: [
+    {
       label: "Mechanical",
       icon: "mdi-wrench",
-      sub: [{
+      sub: [
+        {
           label: "Valves",
           icon: "mdi-reflect-vertical mdi-rotate-90",
-          sub: null
+          sub: null,
         },
         {
           label: "Engine",
           icon: "mdi-engine",
-          sub: [{
-            label: "Sensors",
-            icon: "mdi-thermometer",
-            sub: null
-          }]
-        }
-      ]
+          sub: [
+            {
+              label: "Sensors",
+              icon: "mdi-thermometer",
+              sub: null,
+            },
+          ],
+        },
+      ],
     },
     {
       label: "Electrical",
       icon: "mdi-flash",
-      sub: [{
-        label: "Isolations",
-        icon: "mdi-flash",
-        sub: null
-      }]
+      sub: [
+        {
+          label: "Isolations",
+          icon: "mdi-flash",
+          sub: null,
+        },
+      ],
     },
     {
       label: "Safety",
       icon: "mdi-safety-goggles",
-      sub: [{
-        label: "Emergency Stations",
-        icon: "mdi-bullhorn",
-        sub: null
-      }]
-    }
+      sub: [
+        {
+          label: "Emergency Stations",
+          icon: "mdi-bullhorn",
+          sub: null,
+        },
+      ],
+    },
   ],
-}
+};
 
 function menuBuilder(parent) {
   for (var y in parent.sub) {
@@ -59,15 +67,23 @@ function menuBuilder(parent) {
     newElement3 = document.createElement("i");
     newElement4 = document.createElement("p");
 
-    newElement.setAttribute('class', "w3-col l3 m4 s6 w3-margin-bottom");
+    newElement.setAttribute("class", "w3-col l3 m4 s6 w3-margin-bottom");
 
-    newElement2.setAttribute('class', "w3-card w3-container w3-display-container w3-theme-l5");
-    newElement2.setAttribute('style', "padding-top: 100%");
-    newElement2.setAttribute('onclick', "openTab('" + sub["label"] + "')");
+    newElement2.setAttribute(
+      "class",
+      "w3-card w3-container w3-display-container w3-theme-l5"
+    );
+    newElement2.setAttribute("style", "padding-top: 100%");
+    newElement2.setAttribute("onclick", "openTab('" + sub["label"] + "')");
 
-    newElement3.setAttribute('class', "mdi " + sub["icon"] + " w3-margin-bottom w3-text-theme w3-hover-opacity w3-display-middle w3-jumbo");
+    newElement3.setAttribute(
+      "class",
+      "mdi " +
+        sub["icon"] +
+        " w3-margin-bottom w3-text-theme w3-hover-opacity w3-display-middle w3-jumbo"
+    );
 
-    newElement4.setAttribute('class', "w3-text-theme w3-display-topmiddle");
+    newElement4.setAttribute("class", "w3-text-theme w3-display-topmiddle");
     newElement4.innerHTML = sub["label"];
 
     newElement2.appendChild(newElement3);
@@ -78,9 +94,12 @@ function menuBuilder(parent) {
     p = document.getElementById("main-content");
     newElement = document.createElement("div");
 
-    newElement.setAttribute('class', "w3-row-padding w3-center w3-margin-top tabPage");
-    newElement.setAttribute('style', "display:none");
-    newElement.setAttribute('id', sub["label"]);
+    newElement.setAttribute(
+      "class",
+      "w3-row-padding w3-center w3-margin-top tabPage"
+    );
+    newElement.setAttribute("style", "display:none");
+    newElement.setAttribute("id", sub["label"]);
     p.appendChild(newElement);
 
     if (sub != null) {
@@ -88,7 +107,6 @@ function menuBuilder(parent) {
     }
   }
 }
-
 
 function w3_open() {
   document.getElementById("sideBar").style.display = "block";
@@ -102,21 +120,31 @@ function sendMail() {
   var recipient = "josephdouce";
   var at = String.fromCharCode(64);
   var host = "gmail.com";
-  document.location.href = "mailto:" + recipient + at + host + "?subject=" +
-    encodeURIComponent("Iona Toolbox Support: " + document.getElementById('senderName').value) +
-    "&body=" + encodeURIComponent(document.getElementById('mailBody').value);
+  document.location.href =
+    "mailto:" +
+    recipient +
+    at +
+    host +
+    "?subject=" +
+    encodeURIComponent(
+      "Iona Toolbox Support: " + document.getElementById("senderName").value
+    ) +
+    "&body=" +
+    encodeURIComponent(document.getElementById("mailBody").value);
 }
-
 
 //Encrypt the selected file and display the data
 function encryptFile() {
-  var file = document.querySelector('input[type=file]').files[0];
-  var reader = new FileReader()
+  var file = document.querySelector("input[type=file]").files[0];
+  var reader = new FileReader();
 
   reader.onload = function (event) {
-    var ciphertext = CryptoJS.AES.encrypt(event.target.result, passphrase).toString();
+    var ciphertext = CryptoJS.AES.encrypt(
+      event.target.result,
+      passphrase
+    ).toString();
     document.getElementById("encrypt-display").innerHTML = ciphertext;
-  }
+  };
 
   if (file) {
     reader.readAsText(file);
@@ -127,15 +155,14 @@ function encryptFile() {
 
 //Encrypt the selected file and display the data
 function decryptFile() {
-
-  var file = document.querySelector('input[type=file]').files[0];
-  var reader = new FileReader()
+  var file = document.querySelector("input[type=file]").files[0];
+  var reader = new FileReader();
 
   reader.onload = function (event) {
     var bytes = CryptoJS.AES.decrypt(event.target.result, passphrase);
     var originalText = bytes.toString(CryptoJS.enc.Utf8);
     document.getElementById("encrypt-display").innerHTML = originalText;
-  }
+  };
 
   if (file) {
     reader.readAsText(file);
@@ -146,107 +173,123 @@ function decryptFile() {
 
 // Log in button action
 function login() {
+  passphrase = document.getElementById("login-password").value;
 
-  passphrase = document.getElementById('login-password').value;
+  processFile(
+    "encrypted_csv_files/electrical_isolation_list_encrypted.csv",
+    "Isolations",
+    "selectIsolation",
+    "displayDataElecIsolations",
+    "isolationsSearchInput",
+    elecIsolations
+  );
 
-  processFile('encrypted_csv_files/electrical_isolation_list_encrypted.csv', 
-                  "Isolations", 
-                  "selectIsolation", 
-                  "displayDataElecIsolations", 
-                  "isolationsSearchInput", 
-                  elecIsolations);
+  processFile(
+    "encrypted_csv_files/emergency_stations_encrypted.csv",
+    "Emergency Stations",
+    "selectEmergencyStation",
+    "displayDataEmergencyStations",
+    "emergencyStationsSearchInput",
+    emergencyStations
+  );
 
-  processFile('encrypted_csv_files/emergency_stations_encrypted.csv', 
-                  "Emergency Stations", 
-                  "selectEmergencyStation", 
-                  "displayDataEmergencyStations", 
-                  "emergencyStationsSearchInput", 
-                  emergencyStations);
+  processFile(
+    "encrypted_csv_files/hydraulic_valves_encrypted.csv",
+    "Valves",
+    "selectValve",
+    "displayDataValve",
+    "valveSearchInput",
+    valves
+  );
+  // Fetch encrypted .csv file and process is with papa
 
-  processFile('encrypted_csv_files/hydraulic_valves_encrypted.csv', 
-                  "Valves", 
-                  "selectValve", 
-                  "displayDataValve", 
-                  "valveSearchInput", 
-                  valves);
-      // Fetch encrypted .csv file and process is with papa
-
-      processFile('encrypted_csv_files/hydraulic_valves_encrypted.csv', 
-      "Sensors", 
-      "selectSensor", 
-      "displayDataSensors", 
-      "sensorsSearchInput", 
-      valves);
-// Fetch encrypted .csv file and process is with papa
-
+  processFile(
+    "encrypted_csv_files/engine_sensors_encrypted.csv",
+    "Sensors",
+    "selectSensor",
+    "displayDataSensors",
+    "sensorsSearchInput",
+    sensors
+  );
+  // Fetch encrypted .csv file and process is with papa
 }
 
-function processFile(filePath, tab, selectId, displayId, searchInputId, dataSource) {
-// Fetch encrypted .csv file and process is with papa
-fetch(filePath)
-.then(response => response.text())
-.then((data) => {
-    // Decrypt
-    var bytes = CryptoJS.AES.decrypt(data, passphrase);
-    try {
+function processFile(
+  filePath,
+  tab,
+  selectId,
+  displayId,
+  searchInputId,
+  dataSource
+) {
+  // Fetch encrypted .csv file and process is with papa
+  fetch(filePath)
+    .then((response) => response.text())
+    .then((data) => {
+      // Decrypt
+      var bytes = CryptoJS.AES.decrypt(data, passphrase);
+      try {
         var originalText = bytes.toString(CryptoJS.enc.Utf8);
-    } catch (err) {
-        document.getElementById('passwordPanel').style.display = "block";
+      } catch (err) {
+        document.getElementById("passwordPanel").style.display = "block";
         return;
-    }
+      }
 
-    // Parse decrypted data and phones valves variable 
-    Papa.parse(originalText, {
+      // Parse decrypted data and phones valves variable
+      Papa.parse(originalText, {
         header: true,
-        complete: function(results) {
-            dataSource = results;
-            pageBuilder (tab, selectId, displayId, searchInputId);
-            for (i = 0; i < dataSource.data.length; i++) {
-                var option = document.createElement('option');
-                option.text = dataSource.data[i]["Display Name"];
-                option.value = dataSource.data[i]["Id"];
-                document.getElementById(selectId).add(option);
-            }
-            $('#'+searchInputId).keypress(function(event) { 
-              if (event.keyCode === 13) { 
-                searchData(document.getElementById(searchInputId).value,dataSource,selectId);
-                dropdownSelected(selectId, displayId, dataSource);
-              } 
-            }); 
-            $('#'+selectId).change(function(event) {
-              if (event) {
+        complete: function (results) {
+          dataSource = results;
+          pageBuilder(tab, selectId, displayId, searchInputId);
+          for (i = 0; i < dataSource.data.length; i++) {
+            var option = document.createElement("option");
+            option.text = dataSource.data[i]["Display Name"];
+            option.value = dataSource.data[i]["Id"];
+            document.getElementById(selectId).add(option);
+          }
+          $("#" + searchInputId).keypress(function (event) {
+            if (event.keyCode === 13) {
+              searchData(
+                document.getElementById(searchInputId).value,
+                dataSource,
+                selectId
+              );
               dropdownSelected(selectId, displayId, dataSource);
-              }             
-           }); 
-            dropdownSelected(selectId, displayId, dataSource);
-            document.getElementById("login-page").style.display = "none";
-            document.getElementById('passwordPanel').style.display = "none";
-            document.getElementById("main-content").style.display = "block";
-        }
+            }
+          });
+          $("#" + selectId).change(function (event) {
+            if (event) {
+              dropdownSelected(selectId, displayId, dataSource);
+            }
+          });
+          dropdownSelected(selectId, displayId, dataSource);
+          document.getElementById("login-page").style.display = "none";
+          document.getElementById("passwordPanel").style.display = "none";
+          document.getElementById("main-content").style.display = "block";
+        },
+      });
     });
-});  
 }
 
 // build UI for each page
 
-function pageBuilder (tab, selectId, displayId, searchInputId){
-
+function pageBuilder(tab, selectId, displayId, searchInputId) {
   // search box
   p = document.getElementById(tab);
   newElement = document.createElement("div");
   newElement2 = document.createElement("div");
   newElement3 = document.createElement("input");
 
-  newElement.setAttribute('class', "w3-row-padding");
-  newElement2.setAttribute('class', "w3-col s12 m4 l4");
-  newElement3.setAttribute('class', "w3-input w3-border w3-margin-top");
-  newElement3.setAttribute('placeholder', "Search");
-  newElement3.setAttribute('type', "text");
-  newElement3.setAttribute('id', searchInputId);
+  newElement.setAttribute("class", "w3-row-padding");
+  newElement2.setAttribute("class", "w3-col s12 m4 l4");
+  newElement3.setAttribute("class", "w3-input w3-border w3-margin-top");
+  newElement3.setAttribute("placeholder", "Search");
+  newElement3.setAttribute("type", "text");
+  newElement3.setAttribute("id", searchInputId);
 
   newElement.appendChild(newElement2);
   newElement2.appendChild(newElement3);
-  
+
   p.appendChild(newElement);
 
   // select box
@@ -256,11 +299,11 @@ function pageBuilder (tab, selectId, displayId, searchInputId){
   newElement3 = document.createElement("label");
   newElement4 = document.createElement("select");
 
-  newElement.setAttribute('class', "w3-row-padding");
-  newElement2.setAttribute('class', "w3-col s12 m4 l4");
+  newElement.setAttribute("class", "w3-row-padding");
+  newElement2.setAttribute("class", "w3-col s12 m4 l4");
   newElement3.innerHTML = "Select Equipment";
-  newElement4.setAttribute('class', "w3-select w3-border w3-margin-top");
-  newElement4.setAttribute('id', selectId);
+  newElement4.setAttribute("class", "w3-select w3-border w3-margin-top");
+  newElement4.setAttribute("id", selectId);
 
   newElement.appendChild(newElement2);
   newElement2.appendChild(newElement3);
@@ -272,11 +315,14 @@ function pageBuilder (tab, selectId, displayId, searchInputId){
   newElement = document.createElement("div");
   newElement2 = document.createElement("div");
   newElement4 = document.createElement("p");
-  
-  newElement.setAttribute('class', "w3-row-padding");
-  newElement2.setAttribute('class', "w3-col s12 m12 l12");
-  newElement3.setAttribute('id', displayId);
-  newElement3.setAttribute('class', "data-display w3-col s12 m12 l12 w3-margin-top");
+
+  newElement.setAttribute("class", "w3-row-padding");
+  newElement2.setAttribute("class", "w3-col s12 m12 l12");
+  newElement3.setAttribute("id", displayId);
+  newElement3.setAttribute(
+    "class",
+    "data-display w3-col s12 m12 l12 w3-margin-top"
+  );
 
   newElement.appendChild(newElement2);
   newElement2.appendChild(newElement3);
@@ -284,33 +330,38 @@ function pageBuilder (tab, selectId, displayId, searchInputId){
   p.appendChild(newElement);
 }
 
-function searchData(searchTerm, dataSource, outputDropdown){
+function searchData(searchTerm, dataSource, outputDropdown) {
   document.getElementById(outputDropdown).innerText = null;
   for (i = 0; i < dataSource.data.length; i++) {
-      if (dataSource.data[i]["Display Name"].toLowerCase().includes(searchTerm.toLowerCase())) {
-          var option = document.createElement('option');
-          option.text = dataSource.data[i]["Display Name"];
-          option.value = dataSource.data[i]["Id"];
-          document.getElementById(outputDropdown).add(option);
-      }
+    if (
+      dataSource.data[i]["Display Name"]
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+    ) {
+      var option = document.createElement("option");
+      option.text = dataSource.data[i]["Display Name"];
+      option.value = dataSource.data[i]["Id"];
+      document.getElementById(outputDropdown).add(option);
+    }
   }
 }
 
-function dropdownSelected(selectInput, displayOutput, dataSource){
+function dropdownSelected(selectInput, displayOutput, dataSource) {
   var input = document.getElementById(selectInput).value;
   document.getElementById(displayOutput).innerHTML = "";
   for (i = 0; i < dataSource.data.length; i++) {
-      if (input == dataSource.data[i]["Id"]) {
-          for (var label in dataSource.data[i]) {
-              if(label != "Id" && label != "Display Name"){
-                  document.getElementById(displayOutput).innerHTML += label;
-                  document.getElementById(displayOutput).innerHTML += ": ";
-                  document.getElementById(displayOutput).innerHTML += dataSource.data[i][label];
-                  document.getElementById(displayOutput).innerHTML += "<br>";
-                  document.getElementById(displayOutput).innerHTML += "<br>";
-              }
-          }
+    if (input == dataSource.data[i]["Id"]) {
+      for (var label in dataSource.data[i]) {
+        if (label != "Id" && label != "Display Name") {
+          document.getElementById(displayOutput).innerHTML += label;
+          document.getElementById(displayOutput).innerHTML += ": ";
+          document.getElementById(displayOutput).innerHTML +=
+            dataSource.data[i][label];
+          document.getElementById(displayOutput).innerHTML += "<br>";
+          document.getElementById(displayOutput).innerHTML += "<br>";
+        }
       }
+    }
   }
 }
 
@@ -327,66 +378,70 @@ function openTab(tabName) {
 async function install() {
   if (deferredPrompt) {
     // Hide our user interface that shows our A2HS button
-    document.getElementById('installPanel').style.display = 'none';
+    document.getElementById("installPanel").style.display = "none";
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then(function (choiceResult) {
-
-      if (choiceResult.outcome === 'accepted') {
-        console.log('Your PWA has been installed');
+      if (choiceResult.outcome === "accepted") {
+        console.log("Your PWA has been installed");
       } else {
-        console.log('User chose to not install your PWA');
+        console.log("User chose to not install your PWA");
       }
 
       deferredPrompt = null;
-
     });
   }
 }
 
 // On load function
 async function onLoadFunction() {
-
   document.getElementById("main-content").style.display = "none";
 
-  window.addEventListener('load', function () {
-    window.history.pushState({
-      noBackExitsApp: true
-    }, openTab('Home'))
-  })
+  window.addEventListener("load", function () {
+    window.history.pushState(
+      {
+        noBackExitsApp: true,
+      },
+      openTab("Home")
+    );
+  });
 
-  window.addEventListener('popstate', function (event) {
+  window.addEventListener("popstate", function (event) {
     if (event.state && event.state.noBackExitsApp) {
-      window.history.pushState({
-        noBackExitsApp: true
-      }, openTab('Home'))
+      window.history.pushState(
+        {
+          noBackExitsApp: true,
+        },
+        openTab("Home")
+      );
     }
-  })
+  });
 
   // Webapp install
-  document.getElementById('installPanel').style.display = 'none';
+  document.getElementById("installPanel").style.display = "none";
 
-  window.addEventListener('beforeinstallprompt', (e) => {
-    console.log("[Main] A2HS Triggered")
+  window.addEventListener("beforeinstallprompt", (e) => {
+    console.log("[Main] A2HS Triggered");
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI to notify the user they can add to home screen
-    document.getElementById('installPanel').style.display = 'block';
+    document.getElementById("installPanel").style.display = "block";
   });
 
   // Execute a function when the user releases a key on the keyboard
-  document.getElementById("login-password").addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-      login()
-    }
-  });
+  document
+    .getElementById("login-password")
+    .addEventListener("keyup", function (event) {
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        login();
+      }
+    });
 
   menuBuilder(menuStructure);
-
 }
 
 // Call onload function
